3717a4c0d5bc3891009559a72e83156f436e9215
Patrícia da Silva Ramos <56210981+PatriciaRamosS@users.noreply.github.com>
2022-09-16 12:58:16 -0300

    Aula02

---
+++ Aula02-Variaveis/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome;
+        nome = "Let's Code";
+        System.out.println("Olá, " + nome);
+
+        float a;
+        float b = 2;
+        a = 3;
+
+        float soma = a + b;
+        float subtracao = a - b;
+        float multiplicacao = a * b;
+        float divisao = a / b;
+
+
+        System.out.println("\n");
+        System.out.println("Soma = " + soma) ;
+        System.out.println("Subtração = " + subtracao) ;
+        System.out.println("Divisão = " + multiplicacao) ;
+        System.out.println("Multiplicação = " + divisao) ;
+    }
+}
