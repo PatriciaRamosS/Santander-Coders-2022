28340a4d67ea37ce73b3d4d91a78f8bcfcb89e63
Patrícia da Silva Ramos <56210981+PatriciaRamosS@users.noreply.github.com>
2022-09-17 16:46:55 -0300

    aula06

---
+++ Aula06-Lacos-Numericos/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 0, num=0;
+
+        /*for(i=1;i<=5;i++) {
+            num=num+1;
+            System.out.println(num);
+        }*/
+        
+        //Para cada iteração do i eu terei 10 iterações do j
+        for(i=1;i<=10;i++) {
+            for(int j=1;j<=10;j++) {
+                System.out.println(j + "X" + i + "=" + j*i);
+            }
+
+        }
+    }
+}
